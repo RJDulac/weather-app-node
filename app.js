73d45e81b6fce49c79744ecf28e7e40bd959934f
Ryan Dulac <analyticrye@gmail.com>
2019-02-20 08:39:25 -0500

    yargs configured and address encoded

---
+++ app.js
@@ -0,0 +1,35 @@
+const request = require("request");
+const yargs = require("yargs");
+
+//configure yargs
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+
+console.log(argv);
+
+let encodedAddress = encodeURIComponent("argv.address");
+request(
+  {
+    url: ` http://www.mapquestapi.com/geocoding/v1/address?key=sg9QhdrU1viNICCvBfy4VH2UtBGoWUT6&location=${encodedAddress}`,
+    //tell request this is a json string and convert it
+    json: true
+  },
+  (error, response, body) => {
+    //pretty print object
+    // console.log(JSON.stringify(body, null, 2));
+    // console.log(JSON.stringify(response, null, 2));
+    // console.log(JSON.stringify(error, null, 2));
+    console.log(`Address: ${body.results[0].providedLocation.location}`);
+    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+    console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+  }
+);
